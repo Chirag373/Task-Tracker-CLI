890dedb338e894d25363080002f60a96f40b7036
chirag oza <ozachirag22@gmail.com>
2024-12-05 07:13:28 +0530

    implementation of show file

--- task-cli-program.py
+++ task-cli-program.py
@@ -6,14 +6,7 @@ from datetime import datetime
 TASK_FILE = os.path.abspath("./data.json")
 parser = argparse.ArgumentParser()
 
-# initialize
-input_id = 0
-description = ""
-status = ""
-createdAt = datetime.now()
-updatedAt = datetime.now()
-
-
+# create
 def add_data():
     # input arguments through terminal
     parser.add_argument("-n", "--name", type=str, help="name input")
@@ -26,14 +19,14 @@ def add_data():
         "input_id": 1,
         "description": args.name,
         "status": args.status,
-        "createdAt": createdAt.isoformat(),
-        "updatedAt": updatedAt.isoformat()
+        "createdAt": datetime.now().isoformat(),
+        "updatedAt": datetime.now().isoformat()
     }
 
     # file not exists then create one
     if not os.path.exists(TASK_FILE):
         with open(TASK_FILE, "w") as file:
-            json.dump({"data": []}, file, indent=4)
+            json.dump([], file, indent=4)
             print("No existing data found, creating new file.")
 
     # data handling part
@@ -51,6 +44,28 @@ def add_data():
 
         print(f"Data added successfully: {data}")
 
+# read
+def show_all_data():
+    # read the file
+    with open(TASK_FILE, "r") as file:
+        try:
+            json_data = json.load(file)
+        except json.JSONDecodeError:
+            json_data = {"data": []}
+    
+    data = json_data.get("data")
+    header = data[0].keys()
+
+    print("             |                ".join(header))
+    print("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ")
+    
+    # value handling
+    for value in data:
+        values = value.values()
+        for item in values:
+            print(item, end="   ")
+
+
 
 if __name__ == "__main__":
-    add_data()
+    show_all_data()
